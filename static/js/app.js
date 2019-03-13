9305cf33b771a2f09c373f31c79505f05209037a
Eric Chee <ericcheechee@gmail.com>
2019-03-12 18:26:40 -0700

    working filter search

--- static/js/app.js
+++ static/js/app.js
@@ -24,31 +24,48 @@ function handleClick(){
   d3.select("#filter-btn").on("click", function() {
     d3.event.preventDefault();
     d3.selectAll('td').remove();
+    var filterData = tableData;
 
   var inputValue = d3.select("#datetime").property("value");
     console.log(inputValue);
-  var filterData = tableData.filter(item => item.datetime === inputValue);
-    console.log(filterData);
+    if (inputValue!=""){
+      var filterData = filterData.filter(item => item.datetime === inputValue);
+        console.log(filterData);
+    }
 
   var inputValuecity = d3.select("#city").property("value");
     console.log(inputValuecity);
-  var filterDatacity = tableData.filter(item => item.city === inputValue);
-    console.log(filterDatacity);
+    if (inputValuecity!=""){
+      var filterData = filterData.filter(item => item.city === inputValuecity);
+        console.log(filterData);
+    }
 
-  // var inputValue = d3.select("#country").property("value");
-  //   console.log(inputValue);
-  // var filterData = tableData.filter(item => item.country === inputValue);
-  //   console.log(filterData);
-  //
-  // var inputValue = d3.select("#shape").property("value");
-  //   console.log(inputValue);
-  // var filterData = tableData.filter(item => item.shape === inputValue);
-  //     console.log(filterData);
-  //
-  // var inputValue = d3.select("#comment").property("value");
-  //     console.log(inputValue);
-  // var filterData = tableData.filter(item => item.comment === inputValue);
-  //     console.log(filterData);
+  var inputValuestate = d3.select("#state").property("value");
+    console.log(inputValuestate);
+    if (inputValuestate!=""){
+      var filterData = tableData.filter(item => item.state === inputValuestate);
+        console.log(filterData);
+  }
+
+  var inputValue = d3.select("#country").property("value");
+    console.log(inputValue);
+    if (inputValue!=""){
+      var filterData = tableData.filter(item => item.country === inputValue);
+        console.log(filterData);
+    }
+
+    var inputValue = d3.select("#shape").property("value");
+      console.log(inputValue);
+      if (inputValue!=""){
+        var filterData = tableData.filter(item => item.shape === inputValue);
+          console.log(filterData);
+      }
+      var inputValue = d3.select("#comment").property("value");
+        console.log(inputValue);
+        if (inputValue!=""){
+          var filterData = tableData.filter(item => item.comment === inputValue);
+            console.log(filterData);
+        }
 
   generateTable(filterData);
 
